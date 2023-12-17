271097af0d01be6e82e76569c24676db97f6867c
toiiuvn111 <toiiuvn111@gmail.com>
2023-12-17 21:00:17 +0700

    toan commit nothing here

--- src/main/java/com/db/security/service/EmployeeService.java
+++ src/main/java/com/db/security/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.db.security.model.EmployeeEntity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 public interface EmployeeService {
     public EmployeeEntity getEmployee();
 }
